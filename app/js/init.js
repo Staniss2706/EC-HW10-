0f81725640036a69a1281e29d9c810b910e50fee
Staniss2706 <Staniss2706@ukr.net>
2017-02-13 19:16:01 +0300

    Solicitor (adaptive)

--- app/js/init.js
+++ app/js/init.js
@@ -3,3 +3,11 @@
 //init formstyler
     $('select').styler();
 })(jQuery);
+
+//slicknav
+	$(function(){
+		$('.header-nav').slicknav({
+			label: "",
+			prependTo: ".header .container",
+		});
+	});
